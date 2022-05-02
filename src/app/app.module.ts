2bca83354ff6f4f65225cea1ca07086a2d779d76
Sohan Ubale <sohanubale999@gmail.com>
2022-05-02 20:57:33 +0530

    Initial Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
+import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.component';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantDashComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+@Component({
+  selector: 'app-restaurant-dash',
+  templateUrl: './restaurant-dash.component.html',
+  styleUrls: ['./restaurant-dash.component.css']
+})
+export class RestaurantDashComponent implements OnInit {
+  title = 'restaurantApp';
+   formValue !: FormGroup
+
+  constructor (private formBuilder:FormBuilder) { }
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+    restaurantId: "",
+    name: [""],
+    email: [""],
+    mobile: [""],
+    address: [""],
+    services: [""]
+    })
+  }
+}
+
+
+
+
